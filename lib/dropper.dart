f4ec87b3c6f9d2a52325ce150d87253936a32e6e
Nightmare-MY <906262255@qq.com>
2020-12-01 21:50:27 +0800

    Initial commit

---
+++ lib/dropper.dart
@@ -0,0 +1,4 @@
+library dropper;
+
+export 'src/dropper.dart';
+export 'src/choose_img.dart';
---
+++ lib/src/choose_img.dart
@@ -0,0 +1,80 @@
+import 'dart:io';
+
+import 'package:file_manager/file_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'dropper.dart';
+
+class ChooseImg extends StatefulWidget {
+  @override
+  _ChooseImgState createState() => _ChooseImgState();
+}
+
+class _ChooseImgState extends State<ChooseImg> {
+  String imgPath;
+  @override
+  void initState() {
+    super.initState();
+    test();
+  }
+
+  test() {
+    ProcessResult result = Process.runSync('ls', ['/sdcard']);
+    print(result.stdout);
+    print(result.stderr);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.dark,
+      child: Scaffold(
+        body: Center(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: InkWell(
+              onTap: () async {
+                imgPath = await FileManager.chooseFile(context: context);
+                print('取色器页面返回的文件路径为  $imgPath');
+                // imgPath = await showCustomDialog2<String>(
+                //   context: context,
+                //   isPadding: false,
+                //   height: 600,
+                //   child: FMPage(
+                //     chooseFile: true,
+                //     initpath: '${Global.documentsDir}/YanTool',
+                //     callback: (String path) {
+                //       Navigator.of(context).pop(path);
+                //     },
+                //   ),
+                // );
+                if (imgPath != null) {
+                  Navigator.of(context).push<void>(
+                    MaterialPageRoute<void>(
+                      builder: (BuildContext c) {
+                        return Dropper(
+                          path: imgPath,
+                        );
+                      },
+                    ),
+                  );
+                }
+              },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    '点击页面选择你需要取色的图片',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Icon(Icons.image),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
