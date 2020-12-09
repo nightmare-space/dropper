6ac7cca809a8c8429c544e58c4ca50a7fc00169c
Nightmare-MY <906262255@qq.com>
2020-12-09 15:58:15 +0800

    Update choose_img.dart

--- lib/src/choose_img.dart
+++ lib/src/choose_img.dart
@@ -22,64 +22,88 @@ class _ChooseImgState extends State<ChooseImg> {
   Widget build(BuildContext context) {
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: SystemUiOverlayStyle.dark,
-      child: Scaffold(
-        body: Center(
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: InkWell(
-              onTap: () async {
-                String userPath;
-                if(Platform.isLinux){
-                  userPath='/home/' + Platform.environment['USER'];
-                }else if(Platform.isMacOS){
-                  userPath='/Users/' + Platform.environment['USER'];
-                }else if(Platform.isWindows){
-                  userPath=Platform.environment['USERPROFILE'];
-                  print(Platform.environment['USERPROFILE']);
-                }
-                print(userPath);
-                // return;
-                imgPath = await FileManager.chooseFile(
-                  context: context,
-                  pickPath: userPath,
-                );
-                print('取色器页面返回的文件路径为  $imgPath');
-                // imgPath = await showCustomDialog2<String>(
-                //   context: context,
-                //   isPadding: false,
-                //   height: 600,
-                //   child: FMPage(
-                //     chooseFile: true,
-                //     initpath: '${Global.documentsDir}/YanTool',
-                //     callback: (String path) {
-                //       Navigator.of(context).pop(path);
-                //     },
-                //   ),
-                // );
-                if (imgPath != null) {
-                  Navigator.of(context).push<void>(
-                    MaterialPageRoute<void>(
-                      builder: (BuildContext c) {
-                        return Dropper(
-                          path: imgPath,
-                        );
-                      },
-                    ),
-                  );
-                }
-              },
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    '点击页面选择你需要取色的图片',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Icon(Icons.image),
-                ],
+      child: Theme(
+        data: ThemeData(
+          // 因为file_manager的icon color用的这个颜色
+          accentColor: Colors.teal,
+          appBarTheme: AppBarTheme(
+            color: Colors.white,
+            brightness: Brightness.light,
+            elevation: 0,
+            iconTheme: IconThemeData(
+              color: Colors.black,
+            ),
+            textTheme: TextTheme(
+              headline6: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
               ),
             ),
           ),
+        ),
+        child: Builder(
+          builder: (BuildContext builderContext) {
+            return Scaffold(
+              body: Center(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: InkWell(
+                    onTap: () async {
+                      String userPath;
+                      if (Platform.isLinux) {
+                        userPath = '/home/' + Platform.environment['USER'];
+                      } else if (Platform.isMacOS) {
+                        userPath = '/Users/' + Platform.environment['USER'];
+                      } else if (Platform.isWindows) {
+                        userPath = Platform.environment['USERPROFILE'];
+                      }
+                      print(userPath);
+                      // return;
+                      imgPath = await FileManager.chooseFile(
+                        context: context,
+                        pickPath: userPath,
+                      );
+                      print('取色器页面返回的文件路径为  $imgPath');
+                      // imgPath = await showCustomDialog2<String>(
+                      //   context: context,
+                      //   isPadding: false,
+                      //   height: 600,
+                      //   child: FMPage(
+                      //     chooseFile: true,
+                      //     initpath: '${Global.documentsDir}/YanTool',
+                      //     callback: (String path) {
+                      //       Navigator.of(context).pop(path);
+                      //     },
+                      //   ),
+                      // );
+                      if (imgPath != null) {
+                        Navigator.of(context).push<void>(
+                          MaterialPageRoute<void>(
+                            builder: (BuildContext c) {
+                              return Dropper(
+                                path: imgPath,
+                              );
+                            },
+                          ),
+                        );
+                      }
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          '点击页面选择你需要取色的图片',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Icon(Icons.image),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
